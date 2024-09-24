8ce9b706fd225cdb8fe7a76562da71d715cea3b6
Joshua Bermel <joshua.bermel@icloud.com>
2024-09-24 12:45:06 -0700

    Added line follower node and updated launch file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/fizzer/enph353_ws/src
+# Build directory: /home/fizzer/enph353_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("enph353_ros_lab")
